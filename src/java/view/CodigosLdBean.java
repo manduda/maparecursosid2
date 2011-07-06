870c7913d2f6477736bb54db484c53048d6cb026
maduran@gmail.com <maduran@gmail.com@1cfa86b4-e224-2e77-34e6-ce1c3e2a8e40>
2011-07-06 13:57:25 +0000

    Importando código fuente

---
+++ src/java/view/CodigosLdBean.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import facade.facade;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import vo.ClCodigosLdVO;
+
+/**
+ *
+ * @author juan.loaiza
+ */
+@ManagedBean
+@RequestScoped
+public class CodigosLdBean {
+    
+                
+    private List<ClCodigosLdVO> CoLD = new ArrayList<ClCodigosLdVO>();
+
+    public CodigosLdBean(){
+        facade fachada = new facade();
+
+        CoLD = fachada.ListaCodigosLd();
+    }
+
+    public List<ClCodigosLdVO> getCoLD() {
+        return CoLD;
+    }
+
+    public void setCoLD(List<ClCodigosLdVO> CoLD) {
+        this.CoLD = CoLD;
+    }
+    
+        /** Creates a new instance of CodigosLdBean */
+
+}
