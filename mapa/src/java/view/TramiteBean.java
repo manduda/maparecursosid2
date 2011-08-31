54d7bc7cb43295f084824db3acc3b98820462032
maduran@gmail.com <maduran@gmail.com@1cfa86b4-e224-2e77-34e6-ce1c3e2a8e40>
2011-08-31 20:52:53 +0000

    importando 2011-08-31

--- mapa/src/java/view/TramiteBean.java
+++ mapa/src/java/view/TramiteBean.java
@@ -5,19 +5,25 @@
 package view;
 
 import facade.facade;
+import helper.ConvertirListasHelper;
 import inicio.LoginFilter;
 import inicio.UserBean;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import vo.EmOperadorVO;
+import vo.EtEstadoTramiteVO;
 import vo.GtGestionTramiteVO;
 import vo.TrTramitesVO;
 import vo.UsUsuariosVO;
@@ -27,15 +33,19 @@ import vo.UsUsuariosVO;
  * @author miguel.duran
  */
 @ManagedBean(name = "TramiteBean")
-@RequestScoped
+@SessionScoped
 public class TramiteBean {
     private List<TrTramitesVO> tramites = new ArrayList<TrTramitesVO>();
     private TrTramitesVO selectedTramite;
+    private Collection<SelectItem> listaOperador;
+    private String operadorCrearTramite;
+    private UsUsuariosVO userVO;
+    private String mensajeCrearTramite;
 
     /** Creates a new instance of TramiteBean */
     public TramiteBean() {
         facade fachada = new facade();
-        UsUsuariosVO userVO = new UsUsuariosVO();
+        userVO = new UsUsuariosVO();
         UserBean userSession = null;
 
         FacesContext facesContext = FacesContext.getCurrentInstance();
@@ -71,6 +81,56 @@ public class TramiteBean {
         return null;
     }
     
+    public String opcionesCrearTramite() {
+        facade fachada = new facade();
+        operadorCrearTramite = "-1";
+        mensajeCrearTramite = "";
+        try {
+            ConvertirListasHelper convertir = new ConvertirListasHelper();
+            listaOperador = convertir.createSelectItemsList(fachada.cargarOperadores(), "getEmrCodigo", null, "getEmtNombre", true, "");
+        } catch (Exception e) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, "Error en el bean de Trámites", e);
+        }
+        
+        return "usuarios/crearTramite";
+    }
+    
+    public String crearTramite() {
+        facade fachada = new facade();
+
+        TrTramitesVO vo = new TrTramitesVO();
+        
+        EmOperadorVO operador = new EmOperadorVO();
+        operador.setEmrCodigo(operadorCrearTramite);
+        
+        EtEstadoTramiteVO estado = new EtEstadoTramiteVO();
+        estado.setEtnCodigo(1);
+        
+        UsUsuariosVO usuario = new UsUsuariosVO();
+        usuario.setUsnCodigo(userVO.getUsnCodigo());
+        
+        Date fecha = new Date();
+        
+        vo.setEmrCodigo(operador);
+        vo.setEtnCodigo(estado);
+        vo.setUsnCodigo(usuario);
+        vo.setTrfFecha(fecha);
+   
+        boolean resultado = fachada.crearTramite(vo);
+        
+        System.out.println("resultado: "+resultado);
+        
+        if (resultado == true){
+            tramites = fachada.cargarTramites(6, userVO.getUsnCodigo());
+            mensajeCrearTramite = "Trámite creado.<br><br>Código del trámite: "+tramites.get(0).getTrnCodigo();
+        } else {
+            mensajeCrearTramite = "<b>Error al crear el trámite.</b><br><br>Si el error persiste, por favor contacte al Aministrador";
+        }
+        
+        return null;
+    }
+    
+    
 
     public List<TrTramitesVO> getTramites() {
         return tramites;
@@ -86,6 +146,38 @@ public class TramiteBean {
 
     public void setSelectedTramite(TrTramitesVO selectedTramite) {
         this.selectedTramite = selectedTramite;
+    }
+
+    public Collection<SelectItem> getListaOperador() {
+        return listaOperador;
+    }
+
+    public void setListaOperador(Collection<SelectItem> listaOperador) {
+        this.listaOperador = listaOperador;
+    }
+
+    public String getOperadorCrearTramite() {
+        return operadorCrearTramite;
+    }
+
+    public void setOperadorCrearTramite(String operadorCrearTramite) {
+        this.operadorCrearTramite = operadorCrearTramite;
+    }
+
+    public UsUsuariosVO getUserVO() {
+        return userVO;
+    }
+
+    public void setUserVO(UsUsuariosVO userVO) {
+        this.userVO = userVO;
+    }
+
+    public String getMensajeCrearTramite() {
+        return mensajeCrearTramite;
+    }
+
+    public void setMensajeCrearTramite(String mensajeCrearTramite) {
+        this.mensajeCrearTramite = mensajeCrearTramite;
     }
     
 }
